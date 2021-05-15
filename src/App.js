5d930bca64fa8647384205f8a2040f5a6a7eed81
Yakov Markovich <yakob.markovich@gmail.com>
2021-05-15 18:13:49 +0300

    commit before selectors 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import  {Component} from 'react';
+import Navbar from "./components/navbar/Navbar";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
+import DialogsContainer from "./components/dialogs/DialogsContainer";
+import Users from "./components/users/UsersFC";
+import UsersContainer from "./components/users/UsersContainer";
+import ProfileContainer from "./components/profile/ProfileContainer";
+import HeaderContainer from "./components/header/HeaderContainer";
+import LoginPage from "./components/login/Login";
+import {connect} from "react-redux";
+import {getAuthUserData, logout} from "./redux/auth-reducer";
+import {compose} from "redux";
+import {initializeApp} from "./redux/app-reducer";
+import Preloader from "./components/common/Preloader/Preloader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+
+
+ссылка на git-репозиторий: https://github.com/it-kamasutra/react-way-of-samurai
+
+gitk --all&     (открыть графическую оболочку гита, чтобы позырить все коммиты)
+ctrl + insert (копировать в буфер обмена)
+shift + insert (вставить из буфера обмена)
+git checkout commit-number (переключится на нужный коммит)
+
+*/
+
+
+class App extends Component {
+
+    componentDidMount() {
+        /*authAPI.me()
+             .then(response => {
+                 if(response.data.resultCode===0){
+                     let {id, email, login} = response.data.data;
+                     this.props.setAuthUserData(id, email, login);
+                 }
+             })*/
+        this.props.initializeApp();
+    }
+    render() {
+        if(!this.props.initialized){
+            return <Preloader/>
+        }
+        return (
+            <div className="app-wrapper">
+                <HeaderContainer/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    {/*<Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>*/}
+                    <Route path='/dialogs'
+                           render={ () => <DialogsContainer store={this.props.store}/> }/>
+                    <Route path='/profile/:userId?'
+                           render={() => <ProfileContainer/>}/>
+                    <Route path='/users'
+                           render={() => <UsersContainer/>}/>
+                    <Route path='/login'
+                           render={() => <LoginPage/>}/>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+})
+
+export default compose(
+    withRouter,
+    connect(mapStateToProps, {initializeApp}))(App);
+
+
